e1adfd541af2f54e180231b8222885a06c819391
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-15 11:44:29 -0400

    Add framework for quiz storage and quiz store

---
+++ src/interfacers/quiz-storage.js
@@ -0,0 +1,13 @@
+export async function fetchQuizzes() {}
+
+export async function fetchQuiz() {}
+
+export async function fetchResponse() {}
+
+export async function fetchResponseAndPredecessors() {}
+
+export async function storeQuiz() {}
+
+export async function createResponse() {}
+
+export async function updateResponse() {}
