badb6c2d6418486f725e242b45b482e7f6e669d9
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-14 22:53:36 -0400

    Move OpenAI into a Supabase Edge Function

--- src/interfacers/quiz-builder.js
+++ src/interfacers/quiz-builder.js
@@ -1,6 +1,6 @@
-import { openai } from '@/lib/llm'
+import { getLLMResponse } from '@/lib/llm'
 
-const sourceInformationPrompt = `Your task is to make a set of information on a topic that should summarize the information that someone at a specific level of learning who has studied a topic for a certain amount of time would know.
+const sourceInformationPrompt = `Your task is to make a set of information on a topic that should summarize the information that someone at a specific level of learning who has studied a topic for a certain amount of time would know. Ensure that your language is appropriate for the level of education that the user has attained.
   This knowledge should be separated into sections each with a title, the information, and a summary. These sections should work as if they were sections of a textbook on the topic.
   After all of the sections, you are to add a final summary. Also you need to understand that your work will lead to the creation of quizzes on the topic.
 
@@ -40,18 +40,22 @@ export async function buildSourceInformation(topic, time, grade) {
     { role: 'developer', content: sourceInformationPrompt },
     {
       role: 'user',
-      content: `I want a quiz on ${topic}. I have studied it for ${time}. I have attained a ${grade} level of overall learning`,
+      content: `I want a summary on ${topic}. I have studied it for ${time}. I have attained a ${grade} level of overall learning`,
     },
   ]
 
-  const response = await openai.responses.create({
+  const response = await getLLMResponse({
     model: 'gpt-4o-mini',
-    inputs,
+    input: inputs,
     text: { format: sourceInformationStructure },
   })
 
+  if (response === false) {
+    throw new Error('Error in request, please try again')
+  }
+
   if (response.refusal) {
-    throw Error(`The model refused due to ${response.refusal}`)
+    throw new Error(`The model refused due to ${response.refusal}`)
   }
 
   return JSON.parse(response.output_text)
