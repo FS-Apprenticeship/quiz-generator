c7cd84dc7bbc2c8ce79562e994d1053e08cd15f8
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-15 17:55:57 -0400

    Update Session Manager

---
+++ src/interfacers/session-manager.js
@@ -0,0 +1,23 @@
+import { supabase } from '@/lib/database'
+
+export async function signUpNewUser(email, password) {
+  const { data, error } = await supabase.auth.signUp({ email, password })
+
+  if (error !== null) return { data: undefined, error: 'Error signing up' }
+  return { data: data.user, error: undefined }
+}
+
+export async function signInWithEmail(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+
+  if (error !== null) return { data: undefined, error: 'Error signing in' }
+
+  return { data, error: undefined }
+}
+
+export async function signOut(access_token) {
+  const { error } = await supabase.auth.signOut(access_token)
+
+  if (error !== null) return false
+  return true
+}
