19c23ece9568d78c3bd5a02f7144fabae8934b3c
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-15 22:06:42 -0400

    Add user storage and setup update functions in the store

--- src/interfacers/user-storage.js
+++ src/interfacers/user-storage.js
@@ -1,11 +1,33 @@
-export async function setupUser(id) {
-  id
+import { supabase } from '@/lib/database'
+
+export async function setupUser(id, name, educationLevel) {
+  const { data, error } = supabase
+    .from('users')
+    .insert({
+      id: id,
+      user_name: name,
+      standard_level: educationLevel,
+    })
+    .select()
+
+  if (error !== null) return { data: undefined, error: 'There was an error' }
+  return { data: data, error: undefined }
 }
 
 export async function retrieveUser(id) {
-  id
+  const { data, error } = await supabase.from('users').select('*').eq('id', id)
+
+  if (error !== null) return undefined
+  return data[0]
 }
 
-export async function updateUser(id, data) {
-  id + data
+export async function updateUser(id, updatedInfo) {
+  const { data, error } = await supabase
+    .from('users')
+    .update({ ...updatedInfo })
+    .eq('id', id)
+    .select()
+
+  if (error !== null) return { data: undefined, error: 'There was an error!' }
+  return { data, error: undefined }
 }
--- src/stores/user-store.js
+++ src/stores/user-store.js
@@ -1,5 +1,5 @@
 import { signInWithEmail, signOutUser, signUpNewUser } from '@/interfacers/session-manager'
-import { retrieveUser, setupUser } from '@/interfacers/user-storage'
+import { retrieveUser, setupUser, updateUser } from '@/interfacers/user-storage'
 import { supabase } from '@/lib/database'
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
@@ -15,16 +15,18 @@ export const useUserStore = defineStore('user', () => {
   async function loadUser() {
     const { data, error } = await supabase.auth.refreshSession()
 
-    if (error !== null) return
+    if (error !== null) return false
 
     user.value = data.user
     session.value = data.session
+    userData.value = await retrieveUser(user.value.id)
+    return true
   }
 
   async function signUp(email, password) {
     const { data, error } = await signUpNewUser(email, password)
     if (error !== undefined) return false
-    await setupUser(data.id)
+    await setupUser(data.id, data.email, 'average')
     return true
   }
 
@@ -35,17 +37,42 @@ export const useUserStore = defineStore('user', () => {
     session.value = data.value
     userData.value = await retrieveUser(user.value.id)
 
-    if (userData.value === undefined) await setupUser(data.id)
+    if (userData.value === undefined)
+      userData.value = await setupUser(data.id, data.email, 'average')
+
+    if (userData.value === undefined) {
+      return false
+    }
 
     return true
   }
 
+  async function updateLevelOfEducation(newLevel) {
+    const { data, error } = updateUser(id, { standard_level: newLevel })
+
+    if (error !== undefined) return false
+
+    userData.value = data
+    return true
+  }
+
+  async function updateName(newName) {
+    const { data, error } = updateUser(id, { user_name: newName })
+
+    if (error !== undefined) return false
+
+    userData.value = data
+    return true
+  }
+
   async function signOut() {
-    const success = signOutUser(session.value.access_token)
+    const success = await signOutUser(session.value.access_token)
     if (!success) return false
     user.value = undefined
     userData.value = undefined
     session.value = undefined
+
+    return true
   }
 
   return {
@@ -57,6 +84,8 @@ export const useUserStore = defineStore('user', () => {
     loadUser,
     signUp,
     signIn,
+    updateLevelOfEducation,
+    updateName,
     signOut,
   }
 })
