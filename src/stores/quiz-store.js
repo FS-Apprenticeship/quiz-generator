1c34d0b76ea9a163cdb4ff800951dd286f5ad017
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-15 22:44:12 -0400

    Add the simple fetch functions

--- src/stores/quiz-store.js
+++ src/stores/quiz-store.js
@@ -1,15 +1,35 @@
+import { fetchMostRecentResponse, fetchQuiz, fetchQuizzes } from '@/interfacers/quiz-storage'
+import { useUserStore } from './user-store'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useQuizStore = defineStore('quiz', () => {
+  const user = useUserStore()
   const quizzes = ref([])
   const quiz = ref(undefined)
   const response = ref(undefined)
 
   async function createQuiz() {}
-  async function getQuizzes() {}
-  async function getQuiz() {}
-  async function getResponse() {}
+  async function getQuizzes() {
+    const { data, error } = fetchQuizzes(user.id.value)
+    if (error !== undefined) return false
+    quizzes.value = data
+    return true
+  }
+  async function getQuiz(quizID) {
+    const { data, error } = fetchQuiz(user.id.value, quizID)
+
+    if (error !== undefined) return false
+    quiz.value = data
+    return true
+  }
+  async function getResponse() {
+    const { data, error } = fetchMostRecentResponse(quiz.value.id)
+    if (error !== undefined) return false
+
+    response.value = data
+    return true
+  }
   async function updateResponse() {}
   async function getFeedback() {}
   async function retryMissedQuestions() {}
