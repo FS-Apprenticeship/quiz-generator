805b28e24f4a2af9445a4da33475728647cea106
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-15 22:24:10 -0400

    Layout the quiz store

--- src/stores/quiz-store.js
+++ src/stores/quiz-store.js
@@ -1,3 +1,31 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useQuizStore = defineStore('quiz', () => {})
+export const useQuizStore = defineStore('quiz', () => {
+  const quizzes = ref([])
+  const quiz = ref(undefined)
+  const response = ref(undefined)
+
+  async function createQuiz() {}
+  async function getQuizzes() {}
+  async function getQuiz() {}
+  async function getResponse() {}
+  async function updateResponse() {}
+  async function getFeedback() {}
+  async function retryMissedQuestions() {}
+  async function adaptiveRetry() {}
+
+  return {
+    quizzes,
+    quiz,
+    response,
+    createQuiz,
+    getQuizzes,
+    getQuiz,
+    getResponse,
+    updateResponse,
+    getFeedback,
+    retryMissedQuestions,
+    adaptiveRetry,
+  }
+})
