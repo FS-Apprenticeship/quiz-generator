da9fbdb5e2c26df41d3a9096bad680164cb10539
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2025-10-29 22:06:09 -0400

    Add three helpful components

---
+++ src/components/universals/BackButtonHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue'
+</script>
+
+<template>
+  <header class="absolute top-0 left-0 right-0 flex items-center justify-around pt-2.5 w-dvw">
+    <BaseButton class="absolute left-0 ml-2 px-4" @click="$router.back()">
+      <h1>&lt;</h1>
+    </BaseButton>
+    <h1 class="text-3xl"><slot /></h1>
+  </header>
+</template>
